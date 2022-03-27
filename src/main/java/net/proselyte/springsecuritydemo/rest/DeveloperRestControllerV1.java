0504aa90a41edd2df5d0079c8ebaef28b8f27e34
Serp2015 <serpych@yandex.ru>
2022-03-27 20:53:17 +0400

    Initial commit

---
+++ src/main/java/net/proselyte/springsecuritydemo/rest/DeveloperRestControllerV1.java
@@ -0,0 +1,32 @@
+package net.proselyte.springsecuritydemo.rest;
+
+import net.proselyte.springsecuritydemo.model.Developer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+    private List<Developer> DEVELOPERS = Stream.of(
+            new Developer(1L, "Ivan", "Ivanov"),
+            new Developer(2L, "Sergey", "Sergeev"),
+            new Developer(3L, "Petr", "Petrov")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS
+                .stream()
+                .filter(x-> x.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
